8e5a29198dfba8a47096dcf9d884c843e6b6200c
lassic <ori@forter.com>
2015-06-16 12:44:04 +0300

    add protobuf dependency for travis build

---
+++ install-protobuf.sh
@@ -0,0 +1,5 @@
+ #!/bin/sh
+ set -ex
+ wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz
+ tar -xzvf protobuf-2.5.0.tar.gz
+ cd protobuf-2.5.0 && ./configure --prefix=/usr && make && sudo make install
