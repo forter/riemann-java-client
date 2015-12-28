e60518afe460415aad2c1a61e0fb6d29335cdac2
Dan Barzilay <dan@forter.com>
2015-12-28 11:07:21 +0200

    Remove clojure interface usage

--- src/main/java/com/aphyr/riemann/client/IPromise.java
+++ src/main/java/com/aphyr/riemann/client/IPromise.java
@@ -3,7 +3,7 @@ package com.aphyr.riemann.client;
 import java.util.concurrent.TimeUnit;
 import java.io.IOException;
 
-public interface IPromise<T> extends IDeref {
+public interface IPromise<T> {
   public void deliver(Object value);
   public T deref() throws IOException;
   public T deref(long time, TimeUnit unit) throws IOException;
